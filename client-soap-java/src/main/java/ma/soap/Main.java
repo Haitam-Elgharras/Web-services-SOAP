8544a1c08ee5d003416ef1088ec0cc36f6e8eb3a
Haitam Elgharras <elgharras.haitam@gmail.com>
2024-05-11 00:54:39 +0100

    Add client for test

---
+++ client-soap-java/src/main/java/ma/soap/Main.java
@@ -0,0 +1,12 @@
+package ma.soap;
+
+public class Main {
+    public static void main(String[] args) {
+//        BankService proxy = new BankQWS().getBankServicePort();
+//        System.out.println(proxy.conversion(100));
+//        System.out.println(proxy.getCompte(1));
+//        System.out.println(proxy.listComptes());
+
+
+    }
+}
