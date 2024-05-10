c73ce80a3dedaf4945374f709fd6b22f4cf02044
Haitam Elgharras <elgharras.haitam@gmail.com>
2024-05-11 00:56:59 +0100

    Proxy

--- client-soap-java/src/main/java/ma/soap/Main.java
+++ client-soap-java/src/main/java/ma/soap/Main.java
@@ -2,6 +2,11 @@ package ma.soap;
 
 public class Main {
     public static void main(String[] args) {
+
+        // The proxy must contain the service interface that we get from the wsdl.
+
+
+
 //        BankService proxy = new BankQWS().getBankServicePort();
 //        System.out.println(proxy.conversion(100));
 //        System.out.println(proxy.getCompte(1));
